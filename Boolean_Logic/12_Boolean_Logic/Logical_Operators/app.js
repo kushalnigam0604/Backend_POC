95cd42c5293188020b911a113fbfbc6712504677
kushalnigam0604 <nigamkushal0@gmail.com>
2022-09-27 19:19:33 +0530

    Add files

--- Boolean_Logic/12_Boolean_Logic/Logical_Operators/app.js
+++ Boolean_Logic/12_Boolean_Logic/Logical_Operators/app.js
@@ -17,16 +17,16 @@ if (password.length >= 6 && password.indexOf(' ') === -1) {
 // 10-65 $20
 // 65+ free
 
-const age = 100;
-if ((age >= 0 && age < 5) || age >= 65) {
-    console.log("FREE");
-} else if (age >= 5 && age < 10) {
-    console.log("$10")
-} else if (age >= 10 && age < 65) {
-    console.log("$20")
-} else {
-    console.log("INVALID AGE!")
-}
+// const age = 100;
+// if ((age >= 0 && age < 5) || age >= 65) {
+//     console.log("FREE");
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10")
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20")
+// } else {
+//     console.log("INVALID AGE!")
+// }
 
 // let firstName = prompt("enter your first name");
 // if (!firstName) {
